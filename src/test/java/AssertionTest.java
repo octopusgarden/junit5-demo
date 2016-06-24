bb22b079f549dcca9b757b8b451849f300877bbe
Elisa Natalie <elisa.natalie@smaato.com>
2016-06-24 12:51:19 +0200

    assertion and assumption

--- src/test/java/AssertionTest.java
+++ src/test/java/AssertionTest.java
@@ -6,10 +6,14 @@ class AssertionTest {
 
     @Test
     void assertTestWithLambda() {
-        assertTrue(1 + 1 == 3, () -> "1 plus 1 is not 3");
-        assertFalse(1 + 1 == 2, () -> "1 plus 1 is  2");
-        assertEquals(1 + 1 == 3, true, () -> "1 plus 1 is not 3");
+        assertTrue(1 + 1 == 2, () -> "1 plus 1 is 2");
+        assertFalse(1 + 1 == 3, () -> "1 plus 1 is not 3");
+        assertEquals(1 + 1, 2, () -> "1 plus 1 is 2");
+        assertNotEquals(1 + 1, 3, () -> "1 plus 1 is not 3");
+        assertNull(null, () -> "null is null");
+        assertNotNull(1, () -> "1 is not null");
+        assertSame(1 + 1, 2, () -> "1 plus 1 is 2");
+        assertNotSame(1 + 1, 3, () -> "1 plus 1 is not 3");
     }
-
 
 }
---
+++ src/test/java/AssumptionTest.java
@@ -0,0 +1,29 @@
+import org.junit.gen5.api.Test;
+
+import static org.junit.gen5.api.Assertions.assertEquals;
+import static org.junit.gen5.api.Assumptions.assumeTrue;
+import static org.junit.gen5.api.Assumptions.assumingThat;
+
+public class AssumptionTest {
+
+    @Test
+    void testOnlyOnDeveloperWorkstation() {
+        assumeTrue("CI".equals(System.getenv("ENV")),
+                () -> "Aborting test: not on developer workstation");
+        assertEquals(1 + 1, 3);
+    }
+
+    @Test
+    void testInAllEnvironments() {
+        assumingThat("CI".equals(System.getenv("ENV")),
+                () -> {
+                    // perform these assertions only on the CI server
+                    assertEquals(1 + 1, 2);
+                });
+
+        // perform these assertions in all environments
+        assertEquals("a string", "a string");
+    }
+
+
+}
