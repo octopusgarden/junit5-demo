aa33b797b6a0186da5f340dcfdaa8a38178ddbdd
Elisa Natalie <elisa.natalie@gmail.com>
2016-09-01 00:03:29 +0200

    Default interface

---
+++ src/main/java/Person.java
@@ -0,0 +1,40 @@
+import java.util.Objects;
+
+public class Person implements Comparable {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (this == o) {
+            return 0;
+        }
+
+        return this.getName().compareTo(((Person) o).getName());
+
+    }
+}
--- src/test/java/EqualsContract.java
+++ src/test/java/EqualsContract.java
@@ -4,45 +4,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-interface EqualsContract<T> extends Testable<T> {
+interface EqualsContract<T> {
+    T createValue();
 
-	T createNotEqualValue();
+    T createNotEqualValue();
 
-	@Test
-	default void valueEqualsItself() {
-		T value = createValue();
-		assertEquals(value, value);
-	}
+    @Test
+    default void valueEqualsItself() {
+        T value = createValue();
+        assertEquals(value, value);
+    }
 
-	@Test
-	default void valueDoesNotEqualNull() {
-		T value = createValue();
-		assertFalse(value.equals(null));
-	}
+    @Test
+    default void valueEqualsWhenSameName() {
+        T value1 = createValue();
+        T value2 = createValue();
+        assertEquals(value1, value2);
+    }
 
-	@Test
-	default void valueDoesNotEqualDifferentValue() {
-		T value = createValue();
-		T differentValue = createNotEqualValue();
-		assertNotEquals(value, differentValue);
-		assertNotEquals(differentValue, value);
-	}
+    @Test
+    default void valueDoesNotEqualNull() {
+        T value = createValue();
+        assertFalse(value.equals(null));
+    }
 
-	interface Testable<T> {
-		T createValue();
-	}
+    @Test
+    default void valueDoesNotEqualDifferentValue() {
+        T value = createValue();
+        T differentValue = createNotEqualValue();
+        assertNotEquals(value, differentValue);
+        assertNotEquals(differentValue, value);
+    }
 
-	class StringTests implements EqualsContract<String> {
-
-		@Override
-		public String createValue() {
-			return "foo";
-		}
-
-		@Override
-		public String createNotEqualValue() {
-			return "baz";
-		}
-
-	}
 }
